cda7a2a93b42de14950cea0180795f9d5a121e47
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-11-08 15:32:52 -0300

    components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,6 +5,7 @@ import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
+import { New } from './src/screens/New';
 
 import { THEME } from './src/styles/theme'
 
@@ -19,7 +20,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <SignIn /> : <Loading /> }
+        { fontsLoaded ? <New /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
