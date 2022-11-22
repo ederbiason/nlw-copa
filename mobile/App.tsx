0e3ef822bd52e0cdb70aabc81d6591a4a6f1e98d
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-11-22 15:46:57 -0300

    creating routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,9 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/SignIn';
-import { New } from './src/screens/New';
-import { Find } from './src/screens/Find';
+import { Routes } from "./src/routes";
 
 import { THEME } from './src/styles/theme'
 
@@ -21,7 +19,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <Find /> : <Loading /> }
+        { fontsLoaded ? <Routes /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
