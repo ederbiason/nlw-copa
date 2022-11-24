5d13bbbb00cf8eac069bb262a347c7ad6efc840b
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-11-24 15:55:32 -0300

    fetching created pools

--- mobile/src/components/EmptyPoolList.tsx
+++ mobile/src/components/EmptyPoolList.tsx
@@ -1,14 +1,25 @@
+import { useNavigation } from '@react-navigation/native';
 import { Row, Text, Pressable } from 'native-base';
 
 export function EmptyPoolList() {
+  const { navigate } = useNavigation()
+
   return (
     <Row flexWrap="wrap" justifyContent="center">
       <Text color="white" fontSize="sm" textAlign="center">
         Você ainda não está participando de {'\n'} nenhum bolão, que tal
       </Text>
 
-      <Pressable>
-          <Text textDecorationLine="underline" color="yellow.500" textDecoration="underline">
+      <Pressable
+        onPress={
+          () => navigate('find')
+        }
+      >
+          <Text 
+            textDecorationLine="underline" 
+            color="yellow.500" 
+            textDecoration="underline"
+          >
             buscar um por código
           </Text>
       </Pressable>
@@ -17,7 +28,11 @@ export function EmptyPoolList() {
         ou
       </Text>
 
-      <Pressable>
+      <Pressable
+        onPress={
+          () => navigate('new')
+        }
+      >
         <Text textDecorationLine="underline"  color="yellow.500">
           criar um novo
         </Text>
