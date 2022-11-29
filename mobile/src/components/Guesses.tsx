85e0e2da6a171f09d4e7a03fb992a00fa2825d23
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-11-29 14:51:16 -0300

    confirming guesses

--- mobile/src/components/Guesses.tsx
+++ mobile/src/components/Guesses.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { api } from '../services/api';
 
 import { Game, GameProps } from './Game';
+import { Loading } from './Loading';
 
 interface Props {
   poolId: string;
@@ -37,9 +38,47 @@ export function Guesses({ poolId }: Props) {
     }
   }
 
+  async function handleGuessConfirm(gameId: string) {
+    try { 
+      if(!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
+        return toast.show({
+          title: 'Informe o placar do palpite',
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoints: Number(firstTeamPoints),
+        secondTeamPoints: Number(secondTeamPoints),
+      })
+
+      toast.show({
+        title: 'Palpite realizado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      })
+
+      fetchGames()
+
+    } catch (error) {
+        console.log(error)
+
+        toast.show({
+            title: 'Não foi possível enviar o palpite',
+            placement: 'top',
+            bgColor: 'red.500'
+        })
+    }
+  }
+
   useEffect(() => {
     fetchGames()
   }, [poolId])
+
+  if(isLoading) {
+    return <Loading />
+  }
 
   return (
     <FlatList 
@@ -47,13 +86,13 @@ export function Guesses({ poolId }: Props) {
       keyExtractor={item => item.id} 
       renderItem={( { item } ) => (
         <Game
-          key={item.id}
           data={item}
           setFirstTeamPoints={setFirstTeamPoints}
           setSecondTeamPoints={setSecondTeamPoints}
-          onGuessConfirm={() => {}}
+          onGuessConfirm={() => handleGuessConfirm(item.id)}
         />
       )}
+      _contentContainerStyle={{ pb: 10 }}
     />
   );
 }
