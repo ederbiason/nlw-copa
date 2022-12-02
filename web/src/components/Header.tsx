58b4b4904868b73645175e02d06a626e639424c1
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-12-02 15:22:03 -0300

    navbar

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,9 +1,32 @@
-
+import { MagnifyingGlass, Medal, SoccerBall } from "phosphor-react"
 
 export const Header = () => {
     return (
         <header className="max-w-[1124px] mx-auto">
-            
+            <nav className="">
+                <ul className="flex justify-evenly text-white font-semibold text-2xl">
+                    <li className="py-5 flex items-center justify-center gap-2">
+                        <SoccerBall />
+                        <a href="">
+                            Meus bolões
+                        </a>
+                    </li>
+
+                    <li className="py-5 flex items-center justify-center gap-2">
+                        <Medal />
+                        <a href="">
+                            Ranking
+                        </a>
+                    </li>
+
+                    <li className="py-5 flex items-center justify-center gap-2">
+                        <MagnifyingGlass />
+                        <a href="" className="">
+                            Buscar bolão
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
